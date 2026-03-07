e7927e41de28baf96ee404ba69a07372835431dd
SX-flyinG <outcast9n0k@gmail.com>
2026-03-07 23:23:29 +0200

    Update WEBAPP_URL to point to the new location

--- bunker-bot/bot/config.py
+++ bunker-bot/bot/config.py
@@ -5,7 +5,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8704064363:AAHEBdJO03RYkkftWnJItMugJVxq-Rm3xJA")
 
 # URL твоего Mini App (GitHub Pages, Vercel и т.д.)
-WEBAPP_URL = os.getenv("WEBAPP_URL", "https://sx-flying.github.io/Bunker_tgbot/")
+WEBAPP_URL = os.getenv("WEBAPP_URL", "https://sx-flying.github.io/Bunker_tgbot/bunker-bot/webapp/index.html")
 
 # Максимальный и минимальный размер комнаты
 ROOM_MIN = 8
