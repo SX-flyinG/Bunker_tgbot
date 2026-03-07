d70929f84ff902059d5e5cf7e8776c3b6b7b185e
SX-flyinG <outcast9n0k@gmail.com>
2026-03-07 23:02:44 +0200

    Update WEBAPP_URL to new GitHub Pages link

--- bunker-bot/bot/config.py
+++ bunker-bot/bot/config.py
@@ -5,7 +5,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8704064363:AAHEBdJO03RYkkftWnJItMugJVxq-Rm3xJA")
 
 # URL твоего Mini App (GitHub Pages, Vercel и т.д.)
-WEBAPP_URL = os.getenv("WEBAPP_URL", "http://borschs_bunker_bot.io/")
+WEBAPP_URL = os.getenv("WEBAPP_URL", "https://sx-flying.github.io/Bunker_tgbot/")
 
 # Максимальный и минимальный размер комнаты
 ROOM_MIN = 8
